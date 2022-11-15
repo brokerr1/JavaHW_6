eeaefd885e9d85c520299f1949ac8f10357287d4
brokerr1 <solovievdima1@gmail.com>
2022-11-15 16:32:16 +0300

    first

--- Homework.java
+++ Homework.java
@@ -2,10 +2,9 @@ package Java.HomeWork.JavaHW_6;
 
 import Java.HomeWork.JavaHW_6.data.Notebook;
 
+
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
@@ -19,28 +18,45 @@ public class Homework {
     // 4) Отфильтровать ноутбуки их первоначального множества и вывести проходящие по условиям.
     public static void main(String[] args) {
         Homework hw = new Homework();
-        
-        System.out.println(hw.initListNotebooks());
-    }
-
-    public List<Notebook> initListNotebooks(){
-        List<Notebook> notebooks = new ArrayList<>();
-        notebooks.add(new Notebook("asus", "5089", 8, 256, "win", "black", 10999));
-        notebooks.add(new Notebook("aser", "6941", 8, 512, "linux", "gray", 10999));
-        notebooks.add(new Notebook("lenovo", "9431", 4, 256, "win", "gray", 10999));
-        notebooks.add(new Notebook("asus", "6815", 16, 1024, "win", "black", 10999));
-        notebooks.add(new Notebook("lenovo", "9844", 8, 512, "linux", "black", 10999));
-        notebooks.add(new Notebook("asus", "1864", 8, 512, "win", "white", 10999));
-        return notebooks;
-    }
-
-    public List<Notebook> filter(Map<String, String> params, List<Notebook> notebooks){
-        Map<String, String> map = new HashMap<>();
+        Map<String, String> map = new HashMap<>(); //фильтры
         map.put("ram", "8");
         map.put("ssd", "512");
         map.put("os", "win");
         map.put("color", "black");
-        return map;
+        List<Notebook> notebookslist = hw.initListNotebooks(); //список ноутбуков
+        System.out.println(notebookslist);
+        List<Notebook> filternotebook = hw.filter(map, notebookslist); //отфильтрованные ноутбуки
+        System.out.println(filternotebook);
+    }
+
+
+    public List<Notebook> initListNotebooks(){
+        List<Notebook> notebooks = new ArrayList<>();
+        notebooks.add(new Notebook("asus", "5089", "8", "256", "win", "black", 10999));
+        notebooks.add(new Notebook("aser", "6941", "8", "512", "linux", "gray", 10999));
+        notebooks.add(new Notebook("lenovo", "9431", "4", "256", "win", "gray", 10999));
+        notebooks.add(new Notebook("asus", "6815", "8", "512", "win", "black", 10999));
+        notebooks.add(new Notebook("lenovo", "9844", "4", "512", "linux", "black", 10999));
+        notebooks.add(new Notebook("asus", "1864", "8", "512", "win", "white", 10999));
+        return notebooks;
+    }
+
+    public List<Notebook> filter(Map<String, String> map, List<Notebook> notebooks){
+        List<Notebook> filternotebooks = new ArrayList<>();
+        for (int i = 0; i < notebooks.size(); i++) {
+            if (notebooks.get(i).getColor() == map.get("color")){
+                if (notebooks.get(i).getOs() == map.get("os")){
+                    if (notebooks.get(i).getRam() == map.get("ram")){
+                        if (notebooks.get(i).getSsd() == map.get("ssd")){
+                        filternotebooks.add(notebooks.get(i));
+                        System.out.println(notebooks.get(i));
+                        }
+                    }
+                }
+            }
+        }
+
+        return filternotebooks;
     }
 
 }
--- data/Notebook.java
+++ data/Notebook.java
@@ -4,13 +4,13 @@ package Java.HomeWork.JavaHW_6.data;
 public class Notebook {
     private String firm;
     private String model;
-    private int ram;
-    private int ssd;
+    private String ram;
+    private String ssd;
     private String os;
     private String color;
-    private int cost;
+    private Integer cost;
 
-    public Notebook(String firm,String model,int ram,int ssd,String os,String color,int cost){
+    public Notebook(String firm,String model,String ram,String ssd,String os,String color,Integer cost){
         this.firm = firm;
         this.model = model;
         this.ram =ram;
@@ -22,7 +22,28 @@ public class Notebook {
 
     @Override
     public String toString() {
-        return String.format("Ноутбук:%s %s Цвет: %s ОС: %s ОЗУ: %d Диск: %d Цена: %d",firm, model, color, os, ram, ssd, cost);
+        return String.format("Ноутбук:%s %s Цвет: %s ОС: %s ОЗУ: %s Диск: %s Цена: %d",firm, model, color, os, ram, ssd, cost);
     }
     
+    public String getColor() {
+        return color;
+    }
+    public Integer getCost() {
+        return cost;
+    }
+    public String getFirm() {
+        return firm;
+    }
+    public String getModel() {
+        return model;
+    }
+    public String getOs() {
+        return os;
+    }
+    public String getRam() {
+        return ram;
+    }
+    public String getSsd() {
+        return ssd;
+    }
 }
